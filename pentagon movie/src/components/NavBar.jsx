b47aa3c0cc0a8c1d5f277297ca2ef0d52ec7222e
lennox <lennoxngubo549@gmail.com>
2025-01-30 11:07:41 +0300

    pentagon

--- pentagon movie/src/components/NavBar.jsx
+++ pentagon movie/src/components/NavBar.jsx
@@ -1,1 +1,6 @@
 //lennox
+import React from "react"; import { Link } from "react-router-dom";
+
+function Navbar() { return ( <nav className="navbar"> <Link to="/" className="home">Home</Link> <Link to="/movies" className="movie">Movies</Link> <Link to="/add-movie" className="add movie">Add Movie</Link> <Link to="/about" className="about">About</Link> </nav> ); }
+
+export default Navbar
