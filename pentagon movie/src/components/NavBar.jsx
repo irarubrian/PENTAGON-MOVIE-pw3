e6dc7b12d4334dbc22ecbe6e4f8e16de4740c062
Kohmmz <jeremiahkoome1@gmail.com>
2025-01-31 22:42:48 +0300

    Add Navbar component and update dependencies for routing

--- pentagon movie/src/components/NavBar.jsx
+++ pentagon movie/src/components/NavBar.jsx
@@ -1,1 +1,16 @@
 //lennox
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav>
+      <Link to="/" className='Home'>Home</Link>
+      <Link to="/movies" className="Movies">Movies</Link>
+      <Link to="/add-movie" className="Add Movie">Add Movie</Link>
+      <Link to="/about" className="About">About</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
